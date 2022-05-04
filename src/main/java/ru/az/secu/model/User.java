1ed39cc7fbb1b0a09fa84a63d114b14fc5bbb386
AlexandrZaruchevsky <alexandr.zaruchevsky@gmail.com>
2022-05-04 19:16:30 +0300

    add validation-api

--- src/main/java/ru/az/secu/model/User.java
+++ src/main/java/ru/az/secu/model/User.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -11,29 +13,32 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User extends BaseEntity {
 
-    @Column(name="username")
+    @Column(name = "username")
+    @Size(min = 3, max = 128)
     private String username;
-    @Column(name="password")
+    @Column(name = "password")
+    @Size(min = 3, max = 256)
     private String password;
-    @Column(name="email")
+    @Column(name = "email")
+    @Email
     private String email;
-    @Column(name="active")
+    @Column(name = "active")
     private Boolean active;
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     private Role role;
-    @Column(name="last_name")
+    @Column(name = "last_name")
     private String lastName;
-    @Column(name="first_name")
+    @Column(name = "first_name")
     private String firstName;
-    @Column(name="middle_name")
+    @Column(name = "middle_name")
     private String middleName;
     @Column(name = "activation_code")
     private String activationCode;
 
     @PrePersist
-    private void setDefaultValue(){
-        if(this.active==null) this.active=false;
+    private void setDefaultValue() {
+        if (this.active == null) this.active = false;
     }
 
 }
